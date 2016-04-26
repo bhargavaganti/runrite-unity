fcf6c35483ab56186b8ae1894f511dec3d7df2e7
Charlotte Blanc <charlotte.blanc@dali.dartmouth.edu>
2016-04-26 14:21:02 -0400

    Added Bluetooth Assets Library

--- Assets/BluetoothScript.cs
+++ Assets/BluetoothScript.cs
@@ -1,24 +1,40 @@
 ﻿using UnityEngine;
-using System.Collections;
-
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
 public class BluetoothScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 	
+		Connect ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
-
-
-
-
-
-
-
-	
 	}
+
+	void Connect(){
+		SerialPort BlueToothConnection = new SerialPort();
+		BlueToothConnection.BaudRate = (9600);
+		BlueToothConnection.PortName = "COM4";
+
+		BlueToothConnection.Open();
+		if (BlueToothConnection.IsOpen)
+		{
+			int length = BlueToothConnection.ReadByte ();
+			for (int i = 0; i < length; i++) {
+				print(BlueToothConnection.ReadByte ().ToString ());
+			}
+
+
+		}
+	}
+
+
 }
