f6ffbaded659a7c235a9f7c3ed380f34be949a06
Charlotte Blanc <charlotte.blanc@dali.dartmouth.edu>
2016-05-24 20:09:58 -0400

    data script and footstrike

---
+++ Assets/FootStrike.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootSrike : MonoBehaviour {
+
+	private ArrayList data = new ArrayList();
+	private double mean;
+
+	void FootStike(int[] d){
+		data.Add (d);
+		mean = 0;
+	}
+
+	public void addData(int[] d){
+		data.Add (d);
+		// calculate new mean
+	}
+
+	public ArrayList getData(){
+		return data;
+	}
+
+	public double getMean(){
+		return mean;
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
